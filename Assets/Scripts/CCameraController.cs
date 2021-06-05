d11893a08b5ccf24b8ec2881bfadf1faf9e56fc9
RomanRoschin <thebestflasher@gmail.com>
2021-06-05 16:57:49 +0300

    Remove prefix m_

--- Assets/Scripts/CCameraController.cs
+++ Assets/Scripts/CCameraController.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CCameraController : MonoBehaviour
 {
-    private Transform m_targetView;
+    private Transform targetView;
     private bool isFirstView;
     [SerializeField]
     private GameObject obj1;
     [SerializeField]
     private GameObject obj2;
-    private CMove m_move;
-    private CMove m_look;
+    private CMove move;
+    private CMove look;
     private Vector3 cameraPos;
     private const int maxDistance = 15;
     private const int minDistance = 5;
@@ -20,11 +20,11 @@ public class CCameraController : MonoBehaviour
     // Start
     void Start()
     {
-        m_move = new CMove();
-        m_look = new CMove();
-        m_move.SetActionSpeed(1.5f);
+        move = new CMove();
+        look = new CMove();
+        move.SetActionSpeed(1.5f);
         isFirstView = true;
-        m_targetView = obj1.transform;
+        targetView = obj1.transform;
         cameraPos = new Vector3(0, 2, -10);
     }
 
@@ -32,22 +32,22 @@ public class CCameraController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            Vector3 previousPosition = m_targetView.position;
+            Vector3 previousPosition = targetView.position;
             isFirstView = !isFirstView;
-            if (isFirstView) m_targetView = obj1.transform;
-            else m_targetView = obj2.transform;
-            m_look.SetPositions(previousPosition, m_targetView.position);
-            m_look.StartAction();
+            if (isFirstView) targetView = obj1.transform;
+            else targetView = obj2.transform;
+            look.SetPositions(previousPosition, targetView.position);
+            look.StartAction();
         }
     }
 
     private void ControlDistance()
     {
-        float d = Vector3.Distance(transform.position, m_targetView.position);
+        float d = Vector3.Distance(transform.position, targetView.position);
         if (d > maxDistance || d < minDistance)
         {
-            m_move.SetPositions(transform.position, m_targetView.position + cameraPos);
-            m_move.StartAction();
+            move.SetPositions(transform.position, targetView.position + cameraPos);
+            move.StartAction();
         }
     }
 
@@ -55,19 +55,19 @@ public class CCameraController : MonoBehaviour
     // LateUpdate
     private void LateUpdate()
     {
-        if (m_move.IsActive())
+        if (move.IsActive())
         {
-            m_move.UpdatePosition();
-            transform.position = m_move.GetCurrentPosition();
+            move.UpdatePosition();
+            transform.position = move.GetCurrentPosition();
         }
         else ControlDistance();
 
-        if(m_look.IsActive())
+        if (look.IsActive())
         {
-            m_look.CorrectTargetPosition(m_targetView.position);
-            m_look.UpdatePosition();
-            transform.LookAt(m_look.GetCurrentPosition());
+            look.CorrectTargetPosition(targetView.position);
+            look.UpdatePosition();
+            transform.LookAt(look.GetCurrentPosition());
         }
-        else transform.LookAt(m_targetView.position);
+        else transform.LookAt(targetView.position);
     }
 }
